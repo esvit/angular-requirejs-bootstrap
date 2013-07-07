2d0baa72a0f3b78ffc9db31bacd430f8dffba136
Alex Bogdanov <fellow.bogdanov@gmail.com>
2013-07-07 12:31:18 +0300

    add select2

--- app/js/app.js
+++ app/js/app.js
@@ -2,16 +2,20 @@
 
 require.config({
     shim: {
+        'select2': { deps: ['jquery'] },
+        'angular-ui-select2': { deps: ['select2', 'angular'] }
     },
     paths: {
         'jquery': '../bower_components/jquery/jquery',
-        'angular': '../bower_components/angular/angular'
+        'angular': '../bower_components/angular/angular',
+        'select2': '../../bower_components/select2/select2',
+        'angular-ui-select2': '../../bower_components/angular-ui-select2/src/select2'
     }
 });
 
 
-define(['jquery', 'angular'], function ($) {
+define(['jquery', 'angular', 'main'], function ($) {
 
-    angular.bootstrap(document.documentElement, []);
+    angular.bootstrap(document.documentElement, ['app']);
 
 });
---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+define('main', ['angular-ui-select2'], function() {
+
+    var app = angular.module('app', ['ui.select2'])
+
+    return app;
+});
